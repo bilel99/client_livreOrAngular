5c2cf8787b2c2a1f030717cd4921b750397f051d
bilel99 <bilel.bekkouche@hotmail.fr>
2016-10-30 12:01:55 +0100

    suite du dev

--- app/controllers/CommentairesCtrl.js
+++ app/controllers/CommentairesCtrl.js
@@ -1,5 +1,5 @@
 angular.module('myApp')
-    .controller('CommentairesCtrl', function($scope, CommentaireService){
+    .controller('CommentairesCtrl', function($scope, CommentaireService, $timeout){
 
         /*function get(){
             let query = CommentaireService.query()
@@ -18,10 +18,28 @@ angular.module('myApp')
         get()
 
         $scope.addCommentaire = function(){
-            CommentaireService.save({users_id : '3', content : $scope.content})
-            
-            get()
-            get()
+            if($scope.content === undefined || $scope.content === ''){
+                $('.error_mess').addClass('error')
+                $('.error').html('Le champ "commentaire" est vide :-(').fadeIn()
+
+                $timeout(function(){
+                    $('.error_mess').fadeOut()
+                }, 3000)
+            }else{
+                $scope.insert = CommentaireService.save({users_id : '3', content : $scope.content})
+                
+                $scope.insert.$promise.then(function(data){
+                    $('.success_mess').addClass('success')
+                    $('.success').html(data.Message).fadeIn()
+                })
+
+                $timeout(function(){
+                    $('.success_mess').fadeOut()
+                }, 3000)
+                
+                get()
+                get()
+            }
         }
 
         $scope.editUpdate = function(id){
@@ -30,16 +48,38 @@ angular.module('myApp')
         }
 
         $scope.updateCommentaire = function(){
-            CommentaireService.update({message : this.idn}, {content: this.edit})
+            if(this.edit === undefined || this.edit === ''){
+                $('.error_mess').addClass('error')
+                $('.error').html('Le champ "commentaire" est vide :-(').fadeIn()
 
-            get()
-            get()
+                $timeout(function(){
+                    $('.error_mess').fadeOut()
+                }, 3000)
+            }else{
+                CommentaireService.update({message : this.idn}, {content: this.edit})
+
+                $('.success_mess').addClass('success')
+                $('.success').html('Votre message à bien été modifier').fadeIn()
+
+                $timeout(function(){
+                    $('.success_mess').fadeOut()
+                }, 3000)
+
+                get()
+                get()
+            }
         }
 
         $scope.deleteCommentaire = function(id){
             CommentaireService.delete({message : id})
 
-            get()
-            get()         
+            $('#comment_'+id).fadeOut()
+
+            $('.success_mess').addClass('success')
+            $('.success').html('Votre message à bien été supprimer').fadeIn()
+
+            $timeout(function(){
+                $('.success_mess').fadeOut()
+            }, 3000) 
         }
     })
